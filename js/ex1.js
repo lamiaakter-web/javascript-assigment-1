9f4538d6b97d8feed134d251d27b6438177aeba1
unknown <you@example.com>
2024-11-17 15:02:23 +0600

    first commit

---
+++ js/ex1.js
@@ -0,0 +1,69 @@
+document.getElementById("student_message").innerHTML="Hi, my name is Lamia, I'm 21 years old and I'm learning Javascript";
+
+
+var num1 = 101;
+var num2 = 7;
+var division = num1 / num2;
+console.log("division.toFixed(4)"); 
+document.getElementById("result").innerHTML=division.toFixed(4);
+
+
+
+var phone1 = "988866552";
+var phone2 = "99087612366";
+var phone3 = 876543123;
+
+
+if( phone1.length == "9") {
+        console.log(phone1+ "valid");
+    } else {
+        console.log(phone1 + "invalid");
+    }
+
+
+if( phone2 === "9") {
+            console.log(phone2.length + "valid");
+        } else {
+            console.log(phone2 + "invalid");
+        }
+
+if( phone3 === "9") {
+            console.log(phone3.length + "valid");
+            } else {
+            console.log(phone3 + "invalid");
+            }
+
+var quantity = "25";
+console.log(quantity += quantity);
+
+
+var number = 6;
+console.log( (7+5) / number + 2 );
+
+
+var pressure;
+console.log(pressure);
+
+
+var temperature = null;
+console.log(temperature);
+
+
+var pressure;
+console.log(typeof pressure);
+
+
+var temperature = null;
+console.log(typeof temperature);
+
+var url = "https://www.google.com";
+console.log(url.replace("https://" , ""));
+document.getElementById("url_4").innerHTML= url.replace("https://" , "");
+
+var url = "https://" ;
+
+var url2 = "www.udemy.com" ;
+
+var fullName = url + url2;
+console.log(fullName);
+document.getElementById("url_2").innerHTML = (fullName);
